f809bbf2ab4327c3aaa56a799400367a2b8df846
YOLO-GIT <ahmadsufi345@gmail.com>
2023-12-29 03:55:35 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ["./dist/*.{html,js}"]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('taos/plugin'),
+    require('tailwindcss-animated')
+  ],
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ]
+}
+
